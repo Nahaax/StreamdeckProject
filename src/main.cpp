523efe2dd2e83f2b99df2857e42e56b8e585d24f
Nahaax <romain.delpie@gmail.com>
2022-11-11 18:35:26 +0100

    Modification of the project in order to use PlatformIO to develop the code for the board

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <Keyboard.h>
+#include <KeyboardLayout.h>
+#include <Keyboard_fr_FR.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
